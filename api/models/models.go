4c2e19c3e16eaef5dff30fe07f7215af5e5b5c49
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-12 22:57:12 +0200

    Create models.go

---
+++ api/models/models.go
@@ -0,0 +1,47 @@
+package models
+
+import "errors"
+
+// AdviceArgs contains all the parameters used in the GiveMeAdvice method
+type AdviceRequestArgs struct {
+	Topic  string `json:"topic"`
+	Amount *int   `json:"amount"`
+}
+
+// AdviceResponse struct represents an advice response
+type AdviceResponse struct {
+	AdviceList []string `json:"adviceList"`
+}
+
+func (adr *AdviceRequestArgs) Validate() error {
+
+	if isBlankString(&adr.Topic) {
+		return errors.New("please provide a topic")
+	}
+
+	if isNegativeInt(adr.Amount) {
+		return errors.New("can't perform requests for negative amount")
+	}
+
+	if isZeroInt(adr.Amount) {
+		return errors.New("can't perform requests for zero amount")
+	}
+
+	return nil
+
+}
+
+// If input variable is empty string, returns true
+func isBlankString(s *string) bool {
+	return s != nil && *s == ""
+}
+
+// If input variable is negative, returns true
+func isNegativeInt(n *int) bool {
+	return n != nil && *n < 0
+}
+
+// If input variable is zero, returns true
+func isZeroInt(n *int) bool {
+	return n != nil && *n == 0
+}
