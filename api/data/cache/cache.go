838cea5f5ae131344fcb33be3a3c9501ab9d6639
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-13 12:51:37 +0200

    Create cache.go

---
+++ api/data/cache/cache.go
@@ -0,0 +1,58 @@
+package cache
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/go-redis/redis"
+	"github.com/marcocharlie/advice-app/api/config"
+)
+
+//AdvicesMemory implements the Advices interface using local in memory persistance
+type AdvicesCache struct {
+	redisClient *redis.Client
+}
+
+func NewAdvicesCache(redisClient *redis.Client) AdvicesCache {
+	return AdvicesCache{
+		redisClient: redisClient,
+	}
+}
+
+func (ac *AdvicesCache) GetTopicAdvicesCache(topic string) ([]string, error) {
+
+	var advicesCache []string
+
+	topicAdviceCache, err := ac.redisClient.Get(topic).Result()
+	if err != nil {
+		return advicesCache, err
+	}
+
+	err = json.Unmarshal([]byte(topicAdviceCache), &advicesCache)
+	if err != nil {
+		log.Println(err)
+		return advicesCache, err
+	}
+
+	log.Printf("retrieved local cache for topic %s", topic)
+
+	return advicesCache, nil
+
+}
+
+func (ac *AdvicesCache) SetTopicAdvicesCache(topic string, advices []string) error {
+
+	json, err := json.Marshal(advices)
+	if err != nil {
+		return err
+	}
+
+	_, err = ac.redisClient.Set(topic, json, config.CacheExpirationTime).Result()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("created local cache for topic %s", topic)
+
+	return nil
+}
