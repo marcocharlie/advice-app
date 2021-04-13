f4347a1d2053c03e6e3035d4e09ed26c70b94857
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-13 12:51:26 +0200

    Update main.go

--- api/cmd/jsonrpc/main.go
+++ api/cmd/jsonrpc/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/rpc/v2"
 	"github.com/gorilla/rpc/v2/json2"
@@ -11,13 +13,20 @@ import (
 
 func main() {
 
+	// init redis client
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDRESS"),
+		Password: "",
+		DB:       0,
+	})
+
 	// instantiate a new RPC Server and register appropriate codecs
 	s := rpc.NewServer()
 	s.RegisterCodec(json2.NewCodec(), "application/json")
 	s.RegisterCodec(json2.NewCodec(), "application/json;charset=UTF-8")
 
 	// create a new RPCService and register it to the RPC Server
-	rpcService := NewRPCService()
+	rpcService := NewRPCService(redisClient)
 	s.RegisterService(rpcService, "RPCService")
 
 	// prepare an HTTP Router, pointing the /rpc url to the RPC Server
