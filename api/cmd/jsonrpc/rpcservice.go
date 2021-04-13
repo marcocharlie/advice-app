c8c3e5d8a55e7309899a4fae00cc691717139b3a
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-13 12:51:29 +0200

    Update rpcservice.go

--- api/cmd/jsonrpc/rpcservice.go
+++ api/cmd/jsonrpc/rpcservice.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/go-redis/redis"
+	advisor "github.com/marcocharlie/advice-app/api/internal/advisor"
 	"github.com/marcocharlie/advice-app/api/models"
 )
 
 // RPCService provides some RPC methods useful to get advices
-type RPCService struct{}
+type RPCService struct {
+	redisCLient *redis.Client
+}
 
 // NewRPCService instantiates a new RPCService
-func NewRPCService() *RPCService {
-	return &RPCService{}
+func NewRPCService(redisClient *redis.Client) *RPCService {
+	return &RPCService{
+		redisCLient: redisClient,
+	}
 }
 
 // GiveMeAdvice returns the Advice list matching the given args.Topic and args.Amount, if present, or an error otherwise
-func (s *RPCService) GiveMeAdvice(args *models.AdviceRequestArgs) error {
+func (s *RPCService) GiveMeAdvice(r *http.Request, args *models.AdviceRequestArgs, reply *models.AdviceResponse) error {
+	log.Printf("got GiveMeAdvice request from %s\n", r.RemoteAddr)
+
+	advices, err := advisor.GetAdvice(s.redisCLient, args)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	*reply = *advices
 
 	return nil
 }
