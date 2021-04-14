2cf281ac871df1d7fca00b98e7f1797836fcde10
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-14 19:23:57 +0200

    enable cors

--- api/cmd/jsonrpc/main.go
+++ api/cmd/jsonrpc/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/go-redis/redis"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/rpc/v2"
 	"github.com/gorilla/rpc/v2/json2"
@@ -33,6 +34,11 @@ func main() {
 	r := mux.NewRouter()
 	r.Handle("/api/advice", s)
 
+	// enable cors
+	headersOk := handlers.AllowedHeaders([]string{"Accept", "Accept-Language", "Content-Language", "Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"POST"})
+
 	// start HTTP server on port 5000
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(":5000", handlers.CORS(originsOk, headersOk, methodsOk)(r)))
 }
--- api/go.mod
+++ api/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/rpc v1.2.0
 	go.opentelemetry.io/otel v0.16.0
